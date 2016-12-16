abecc0107bcec122f59ccf5fc0c778b455f7a98d
hanlchri <hanlchri@shenet.org>
2016-12-16 12:56:14 -0500

    Add files via upload

---
+++ ShenArchitectureHouseProject/src/ShenHauntedApp.java
@@ -0,0 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * S-h-e-n-e-n-d-e-h-o-w-a--H-i-g-h--S-c-h-o-o-l--T-e-c-h-n-o-l-o-g-y--D-e-p-t
+ * ---------------------------------------------------------------------------
+ * FILE: ShenHauntedApp.java
+ *
+ * DATE:  Nov 30, 2016
+ * PURPOSE: App to launch frame for Shen Architecture
+ * Mansion Project for Mr. Guba
+ *
+ * @author mr Hanley
+ * @version 1.0
+ * ---------------------------------------------------------------------------
+ *
+ * h-a-n-l-e-y.c-o-.-n-r------t-e-a-m-2-0-.-c-o-m-----------------------------
+ */
+public class ShenHauntedApp {
+    public static void main(String[] args) {
+        ShenHauntedFrame f = new ShenHauntedFrame();
+        f.setSize(1020,700);
+        f.setVisible(true);
+    }
+}
